de61cdf62b3d2a204db7aa3e9508846214050714
ashishh-tomar <ashishtomar692@gmail.com>
2023-06-30 10:47:42 +0530

    Added Login  component and global styling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
+
+
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Routes>     
+          <Route exact  path='/' element={<Login />}>
+            
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from 'styled-components'
+
+const Login = (props) => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
